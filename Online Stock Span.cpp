3aff44f56336d5103d381792d9e7a4d70733efd9
Akshat Jain <101265586+Akshat2Jain@users.noreply.github.com>
2023-07-05 15:22:31 +0530

    Create Online Stock Span.cpp

---
+++ Online Stock Span.cpp
@@ -0,0 +1,21 @@
+int next(int price) {
+        
+         if(s.empty()){
+           s.push({price, i++});
+           return 1;
+       }
+        else{
+            
+            
+            while(!s.empty() && s.top().first <= price ){
+                s.pop();
+            }
+            int ans;
+            
+            if(s.empty()) ans = i+1;
+            else{
+                ans = i- s.top().second;
+            }
+            s.push({price, i++});
+            return ans;
+        }
