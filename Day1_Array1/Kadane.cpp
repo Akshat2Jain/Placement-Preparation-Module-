321b05318d2be2775679a46c155e07d57e85a41b
akshat2jain <akshat.2024it1031@kiet.edu>
2023-07-22 12:26:52 +0530

    day 1

---
+++ Day1_Array1/Kadane.cpp
@@ -0,0 +1,19 @@
+int maxSubArray(vector<int> &nums)
+{
+    int n = nums.size();
+    int currentsum = 0;
+    int maxsum = INT_MIN;
+    for (int i = 0; i < n; i++)
+    {
+        currentsum = currentsum + nums[i];
+        if (currentsum > maxsum)
+        {
+            maxsum = currentsum;
+        }
+        if (currentsum < 0)
+        {
+            currentsum = 0;
+        }
+    }
+    return maxsum;
+}
